b300a1ea51aa657069e917f0d86fa4a4f6773e10
Mikhail Altunin <altunin@list.ru>
2023-03-29 17:12:51 +0300

    ver 2.0.1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -19,9 +19,9 @@ public class CarController {
     }
 
     @GetMapping()
-    public String index(@RequestParam(value = "count", required = false, defaultValue = "0") int count,
+    public String index(@RequestParam(value = "count", required = false, defaultValue = "-1") int count,
                         Model model) {
-        if ((carService.getAmount() != count) && (count != 0)) {
+        if ((carService.getAmount() != count) && (count != -1)) {
             model.addAttribute("cars", carService.getCountItems(count));
         } else {
             model.addAttribute("cars", carService.getCountItems(carService.getAmount()));
